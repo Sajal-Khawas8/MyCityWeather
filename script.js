1043c33792278ecf81cb33f0278928f1d20276eb
Sajal-Khawas8 <99525286+Sajal-Khawas8@users.noreply.github.com>
2022-12-30 22:02:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function getWeather(city) {
                                 <td>${response.forecast[i].moonset === null ? "-" : response.forecast[i].moonset}</td>
                                 <td>${response.forecast[i].minRelHumidity}%</td>
                                 <td>${response.forecast[i].maxRelHumidity}%</td>
-                                <td>${response.forecast[i].maxWindSpeed} m/s ${directions[Math.round(response.forecast[i].windDir / 45) % 8]}</td>
+                                <td>${response.forecast[i].maxWindSpeed} km/s ${directions[Math.round(response.forecast[i].windDir / 45) % 8]}</td>
                                 <td>${response.forecast[i].minVisibility} m</td>
                                 <td>${response.forecast[i].pressure} hPa</td>
                                 <td>${response.forecast[i].uvIndex}</td>
